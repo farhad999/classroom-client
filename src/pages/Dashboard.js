46a78a06dd6bd2456009b04006dec3218cb18734
Farhad Shikder <frd1952@gmail.com>
2022-09-24 00:30:05 +0600

    Show Routine in dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,38 @@
 import React from "react";
+import axios from "axios";
+import {Button, Card, CardContent, Typography} from "@mui/material";
+import {Link} from 'react-router-dom'
 
 function Dashboard() {
 
+    const [loading, setLoading] = React.useState(true);
+
+    const [routine, setRoutine] = React.useState();
+
+    React.useEffect(()=> {
+        axios.get('/home')
+            .then(res=> {
+                setLoading(false);
+
+                let {routine} = res.data;
+                setRoutine(routine);
+
+            }).catch(er=>console.log(er))
+    }, []);
+
+    if(loading){
+        return <div>loading...</div>
+    }
+
     return (
         <div>
-            Home page
-            {/* Todo show priority based unpublished routine for teacher here */}
+            <Card>
+                <CardContent>
+                    <Typography>{routine.name}</Typography>
+                    <Typography>Priority base routine</Typography>
+                    <Button component={Link} to={`/routines/${routine.id}`} variant={'contained'}>Add your Class</Button>
+                </CardContent>
+            </Card>
         </div>
     )
 }
