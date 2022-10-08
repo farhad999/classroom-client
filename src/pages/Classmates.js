a31c61372c50ad927a95b3a23e98d110e3a55588
Farhad Shikder <frd1952@gmail.com>
2022-10-09 01:02:23 +0600

    start messaging and layout issue fix

--- src/pages/Classmates.js
+++ src/pages/Classmates.js
@@ -2,10 +2,13 @@ import React from 'react'
 import axios from "axios";
 import {Avatar, Box, Card, CardContent, Grid, IconButton, Typography} from "@mui/material";
 import {Message as MessageIcon, MoreHoriz} from "@mui/icons-material";
+import {useNavigate} from "react-router-dom";
 
 function Classmates() {
 
     const [mates, setMates] = React.useState([]);
+
+    const navigate = useNavigate();
 
     React.useEffect(() => {
         axios.get('/mates')
@@ -13,6 +16,16 @@ function Classmates() {
                 setMates(res.data)
             })
     }, []);
+
+    const startMessaging = (id) => {
+        axios.post(`/m/start`, {receiverId: id})
+            .then(res=>{
+                let {status, conversationId} = res.data;
+                if(status === 'success'){
+                    navigate(`/m/${conversationId}`)
+                }
+            })
+    }
 
     return (
         <>
@@ -22,7 +35,7 @@ function Classmates() {
             <Grid container={true} spacing={2}>
                 {
                     mates.map((mate, index) => (
-                        <Grid key={index} lg={4} md={6} item>
+                        <Grid key={index} lg={4} sm={6} xs={12} item>
                             <Card>
                                 <CardContent>
                                     <Box display={'flex'} alignItems={'center'} justifyContent={'space-between'}>
@@ -35,7 +48,7 @@ function Classmates() {
                                                 <Typography>{mate.firstName + ' ' + mate.lastName}</Typography>
                                             </Box>
                                         </Box>
-                                        <IconButton>
+                                        <IconButton onClick={()=>startMessaging(mate.id)}>
                                             <MessageIcon/>
                                         </IconButton>
                                     </Box>
